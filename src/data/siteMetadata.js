381a1855b39a3289b73a48e3f4ec569f3b504bc3
zhao-stanley <contact@stanleyweb.dev>
2022-07-11 00:08:02 -0400

    Metadata update

--- src/data/siteMetadata.js
+++ src/data/siteMetadata.js
@@ -2,9 +2,9 @@ const siteMetadata = {
   title: "Roslyn Code Club",
   desc: "Welcome to the Roslyn Code Club! Here, you can create projects, develop a new hobby, and have fun learning how to code.",
   themeColor: "#5d78fc",
-  url: "https://rhscode.tech/",
-  previewImg: "https://rhscode.tech/img/preview.png",
-  license: "https://github.com/"
+  url: "https://roslyncode.vercel.app/",
+  previewImg: "https://roslyncode.vercel.app/img/preview.png",
+  license: "https://github.com/Roslyn-Code-Club/website/blob/main/LICENSE",
 };
 
 export default siteMetadata;
