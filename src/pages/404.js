b6ee5a1cac2d720a713b834b46b188372c26806c
zhao-stanley <stanleyhzhao@gmail.com>
2022-09-05 13:24:39 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
 
-export default function Login() {
+export default function Page404() {
   const router = useRouter();
   const [time, setTime] = useState(10);
 
