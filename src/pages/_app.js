d5c253aa21210abe798910313efcd9a6172b7c23
Stanley Zhao <stanleyhzhao@gmail.com>
2022-09-14 09:10:00 -0400

    Progress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,19 @@ import "../../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Navbar from "../components/global/Navbar";
 import SEO from "../components/global/SEO";
+import NProgress from "nprogress"
+import Router from "next/router"
 import Footer from "../components/global/Footer";
 import { SessionProvider } from "next-auth/react"
+
+NProgress.configure({ showSpinner: false });
+Router.onRouteChangeStart = url => {
+  NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => NProgress.done()
+
+Router.onRouteChangeError = () => NProgress.done()
 
 export default function App({ Component, pageProps, session }) {
   return (
