3ec76074a7912a1ed46b4987ccfb73bc8a3b280e
zhao-stanley <stanleyhzhao@gmail.com>
2023-03-24 23:39:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,22 @@ const nextConfig = {
     return config;
   },
   images: {
-    domains: ["lh3.googleusercontent.com", "cdn.jsdelivr.net", "docs.google.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "cdn.jsdelivr.net",
+      "docs.google.com",
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/exit",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSfmQ-yCuV8epQO8IkHSMSU2FKpKs4sTBNIc_OeWHfM-iOUWeg/viewform",
+        permanent: false,
+        basePath: false,
+      },
+    ];
   },
 };
 
