bf961a909fd0d50f1ae95b92acde64647ab71f09
Aryan <akaryan519@gmail.com>
2024-03-02 15:50:40 -0500

    people choice

--- src/pages/hackathon/2023.js
+++ src/pages/hackathon/2023.js
@@ -23,8 +23,8 @@ export default function Hackathon() {
           </h1>
           <section className="flex flex-row justify-center gap-4 2xl:gap-16 w-fit">
             <Link href="https://forms.gle/xSk9wGR2TxLkmWPy7">
-              <span className="w-fit text-center cursor-pointer p-3 lg:px-6 border-2 text-white border-white hover:border-roslyn rounded-xl bg-white bg-opacity-10 backdrop-blur-md hover:bg-roslyn font-semibold text-sm md:text-base lg:text-lg 2xl:text-xl transition ease-linear shadow-gray-700 shadow-lg hover:shadow-[#5d77fcb7]">
-                Vote People's Choice
+            <span className="w-fit text-center cursor-pointer p-3 lg:px-6 border-2 text-white border-white hover:border-roslyn rounded-xl bg-white bg-opacity-10 backdrop-blur-md hover:bg-roslyn font-semibold text-sm md:text-base lg:text-lg 2xl:text-xl transition ease-linear shadow-gray-700 shadow-lg hover:shadow-[#5d77fcb7]">
+                People's Choice
               </span>
             </Link>
             <Link href="https://docs.google.com/forms/d/e/1FAIpQLSe3PSbPFjdlSOX6nGllSFpq13NlOSUPzgqS-o9PxPnjoIE6xQ/viewform">
